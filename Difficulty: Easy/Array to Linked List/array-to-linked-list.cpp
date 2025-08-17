52e48ccddc9b82473aa51a7b12c3de199bb34352
Mukesh <149927808+mukkss@users.noreply.github.com>
2025-08-17 19:53:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -0,0 +1,34 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        Node* head = new Node(arr[0]);
+        Node* curr = head;
+        for(int i = 1;i < arr.size(); i++){
+            curr->next = new Node(arr[i]);
+            curr = curr->next;
+        }
+        return head;
+    }
+};
